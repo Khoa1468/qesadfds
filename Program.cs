d9cf02b872e0e93eb82ce9bb76a6f3c8b2c8e4a7
SU <truongthanhthuy11011977@gmail.com>
2020-10-22 15:01:20 +0700

    Source File Program.cs

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+	class CSharp
+	{
+		static void Main(string[] args)
+		{ 
+			#region Console.Write
+				// Console.Write("Hello World!");
+				// Console.Write(10);
+				// Console.Write(10.2f);
+				// Console.Write(true);
+			#endregion
+			#region Console.WriteLine
+				Console.Write("Hello World! \n");
+				Console.WriteLine(5);
+				Console.Write(10.4f);
+				Console.Write(Environment.NewLine);
+				Console.Write(true);
+			#endregion
+			Console.ReadLine();
+		}
+	}
+}
